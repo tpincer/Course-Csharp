88adc0c255a49c7b3a168b2939d3e7863726842f
Tiago Alexandre Pincer da Silva <tiago_pincer@hotmail.com>
2019-11-25 21:04:40 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Entities/Order.cs
@@ -0,0 +1,59 @@
+﻿using Course.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities {
+    class Order {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client) {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AdicionarItem(OrderItem item) {
+            Items.Add(item);
+        }
+
+        public void RemoverItem(OrderItem item) {
+            Items.Remove(item);
+        }
+
+        public double Total() {
+            double sum = 0.0;
+            foreach (OrderItem item in Items) {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order Status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order Items:");
+            foreach (OrderItem item in Items) {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+
+    }
+
+
+}
